1a9764bd499a4536bb39b6616f72a45cc26ec122
Amanuel <shimeliswondimu@yahoo.com>
2022-10-27 23:45:40 -0400

    deploy

---
+++ apps/places/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from apps.category.models  import Category 
+from cloudinary.models import CloudinaryField
+
+# Create your models here.
+
+
+class Places(models.Model): 
+
+    name = models.CharField(
+        'Name', blank=False, null=False, max_length=120, db_index=True
+    )
+    image = CloudinaryField(
+        'image', blank=True, null=True
+    )
+ 
+    Category = models.ForeignKey(
+        Category, on_delete=models.CASCADE
+    )
+    decription = models.CharField(
+        'decription', blank=False, null=False, max_length=500, db_index=True)
+    googel_map_link=models.CharField(
+        'Google Map Link', max_length=500, null=False, blank=False
+    )
+    created_at = models.DateTimeField(
+        'Created Datetime', blank=True, auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        'Updated Datetime', blank=True, auto_now=True
+    )
